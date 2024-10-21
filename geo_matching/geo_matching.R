7e93bc04014874ea7be247d13fbb6854727fbf22
Alessandro Fusta Moro <alessandro.fustamoro@gmail.com>
2024-10-21 23:02:05 +0200

    update

--- geo_matching/geo_matching.R
+++ geo_matching/geo_matching.R
@@ -1,54 +1,39 @@
-library(httr)
-setwd("geo_tools/geo_matching")
-path<-"https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/"
-years <- 2002:2024
-for (y in years) {
-  url <- paste0(
-    "Limiti0101",
-    y,".zip"
-  )
-  if(y %in% c(2001,2021)){
-    url <- paste0(
-      path,"Limiti",
-      y,".zip"
-    )
+geo_matching <- function(data,
+                         format = NULL) {
+  if (is.null(format)) {
+    format <- list("xyt","xyt")
   }
-  if(y == 2011){
-    url <- paste0(
-      path,y,"/Limiti_",
-      y,"_WGS84.zip"
+  #data[[1]]
+  if (format[[1]] == "xyt") {
+    grid1.df <- data[[1]]
+    names(grid1.df) <- c("longitude", "latitude", "time", "var1")
+  } else if (format1 == "grid") { #da matrice a xyt
+    grid1.df <- data.frame(
+      longitude = rep(rep(dimnames(data[[1]])[[2]], each = dim(data[[1]])[1]), dim(data[[1]])[3]),
+      latitude = rep(dimnames(data[[1]])[[1]], dim(data[[1]])[2] * dim(data[[1]])[3]),
+      time = rep(as.Date(as.numeric(dimnames(
+        data[[1]]
+      )[[3]])), each = dim(data[[1]])[2] * dim(data[[1]])[1]),
+      var1 = c(data[[1]])
     )
+  } else{
+    stop("format 1 unknwon")
   }
-  if(y > 2021){
-    url <- paste0(
-      path,y,"/Limiti0101",y,".zip"
+  #data[[2]]
+  if (format[[2]] == "xyt") {
+    grid2.df <- data[[2]]
+    names(grid2.df) <- c("longitude", "latitude", "time", "var2")
+  } else if (format2 == "grid") {
+    grid2.df <- data.frame(
+      longitude = rep(rep(dimnames(data[[2]])[[2]], each = dim(data[[2]])[1]), dim(data[[2]])[3]),
+      latitude = rep(dimnames(data[[2]])[[1]], dim(data[[2]])[2] * dim(data[[2]])[3]),
+      time = rep(as.Date(as.numeric(dimnames(
+        data[[2]]
+      )[[3]])), each = dim(data[[2]])[2] * dim(data[[2]])[1]),
+      var2 = c(data[[2]])
     )
+  } else{
+    stop("format 2 unknwon")
   }
-  name_file <- paste0("dati/confini/zip/Limiti0101",y,".zip")
-  download.file(url = url,
-                destfile = name_file)
-  unzip(zipfile = paste0("dati/confini/zip/Limiti0101",y,".zip"),
-        exdir = "dati/confini/extract_zip/"
-        )
-}
-
-
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/2022/Limiti01012022.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti2021.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012019.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012014.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012003.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012007.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012002.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/2011/Limiti_2011_WGS84.zip
-# https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012017.zip
-
-# Aprire i comuni e fare un STFDF
-lf <- list.files(path="dati/confini/extract_zip")
-for (y in 2001:2024) {
-  lf1 <- grepl(y,lf)
   
-  # list.files(paste0("dati/confini/extract_zip/"), # DA QUI 15/10/2024
 }
-
-# Dati meteorologici
