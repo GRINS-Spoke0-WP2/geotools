68331c79e7f648148b95643a012819b7870bf4fe
Lorenzo Leoni <lorenzo.leoni@tesmec.com>
2025-11-22 16:56:25 +0100

    Fix namespace for hr_grid_LAUs in geomatching function

--- R/geomatching.R
+++ R/geomatching.R
@@ -343,7 +343,7 @@ geomatching <- function(data,
   grid_list <- list()
   i <- 1
   for(date_i in seq(as.Date(first_date), as.Date(last_date), by = "day")){
-    temp <- hr_grid_LAUs
+    temp <- geotools::hr_grid_LAUs
     temp$time <- date_i
     grid_list[[i]] <- temp
     i <- i + 1
